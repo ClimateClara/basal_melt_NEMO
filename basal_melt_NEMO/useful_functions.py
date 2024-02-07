fb045b1560b072f5ae6bdf4a03b983fe8ab06fc7
Clara Burgard <clara.burgard@gmail.com>
2024-02-07 18:02:47 +0100

    reworked on the script to do what VALSO does and much happier now, cdo was jsut not doing the right stuff

--- basal_melt_NEMO/useful_functions.py
+++ basal_melt_NEMO/useful_functions.py
@@ -1,2 +1,7 @@
 def in_range(in_xy,txy):
     return ((in_xy >= min(txy)) & (in_xy <= max(txy)))
+
+
+def weighted_mean(data, dims, weights):
+    weight_sum = weights.sum(dim=dims) # to avoid dividing by zero
+    return (data*weights).sum(dim=dims)/weight_sum.where(weight_sum != 0)
